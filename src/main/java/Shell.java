1fc5b233a7a08a69648910327880204c93127470
Jeffrey Jose <jeffreyjose.k@gmail.com>
2026-05-26 15:51:56 +0530

    codecrafters submit [skip ci]

--- src/main/java/Shell.java
+++ src/main/java/Shell.java
@@ -6,22 +6,21 @@ public class Shell {
     private final CommandRegistry registry = new CommandRegistry();
 
     public void run() {
-        while (true) {
-            Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
+        while (true) {
             System.out.print("$ ");
             System.out.flush();
 
+            if (!scanner.hasNextLine()) break;
             String input = scanner.nextLine().trim();
             String[] parts = input.split(" ", 2);
             String commandName = parts[0];
 
             registry.find(commandName)
-                    .ifPresentOrElse(cmd -> cmd.execute(parts.length > 1 ? parts[1].split(" ") : new String[0]),
+                    .ifPresentOrElse(
+                            cmd -> cmd.execute(parts.length > 1 ? parts[1].split(" ") : new String[0]),
                             () -> System.out.println(commandName + ": command not found"));
-
-            scanner.close();
         }
-
     }
 }
